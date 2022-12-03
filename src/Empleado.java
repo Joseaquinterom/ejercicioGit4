87651439f0adad90832b4c5ac6d529fcb74b4fee
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2022-12-03 18:04:52 +0100

    Encapsulando toString de Empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -10,5 +10,11 @@ class Empleado {
     Empleado(String name, Date birthDate) {
         this.nombre = name;
         this.birthDate = birthDate;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,6 @@ public class Main {
 
 
         Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
-        System.out.println("Empleado: " + p.nombre + " (nacido en 19" + p.birthDate.getYear() + ")");
+        System.out.println(p);
     }
 }
