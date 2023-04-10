6246ae867dcea47ba7f143ec1149598396676a5b
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2023-04-10 02:39:45 +0200

    probando un objeto Persona

---
+++ src/Persona.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: José Antonio Quintero Maya
+ */
+import java.util.Date;
+
+    public class Persona {
+        String nombre;
+        int edad;
+        Date fechaContratacion;
+}
