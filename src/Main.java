1d5e1e466bdd226680e264639d632eca4c3c9bc5
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2023-04-10 04:34:00 +0200

    diseñamos las pruebas de esta funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@
 
 import java.util.Calendar;
 import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
 
         Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
+
+
+        p.nombre = "Manolo"; // Cambio de nombre. No se debe permitir
+        p.fechaContratacion = new Date(99, Calendar.FEBRUARY, 1); // Cambio de fecha. No se debe permitir
     }
 }
