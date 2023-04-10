0fec0e8c45df9cf16216fdbfa65dd4d1144cbbe5
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2023-04-10 06:49:10 +0200

    bug_deprecated_data_type

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,16 @@ public class Main {
         Empleado p2 = Empleado.crearEmpleado("Pepe Pérez", -31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p2);
          */
-
-
-
         System.out.println("Si quiero mostrar solo el nombre: " + p.getNombre());
         System.out.println("Si quiero mostrar solo la edad: " + p.getEdad());
+
+        /* Aunque la variable fechaContratación es inmutable (final), su tipo (Date) es mutable, de modo que
+        todavía podría modificarse así: */
+        p.getFechaContratacion().setYear(10);
+
+        /* Además, Date es una clase de la primera versión de Java que solo almacena dos dígitos para años, de modo
+        que al ponerle el año 10 pensando en 2010, nos encontraremos que no sabemos si se refiere a 1910. */
+        System.out.println("Nueva fecha de contratación: " + p.getFechaContratacion().getYear());
 
     }
 }
